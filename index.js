a3546da3c5d67b45879f7dcfb919983a77c91b11
Maks Kowalski <71663613+kasrow12@users.noreply.github.com>
2024-10-03 01:08:16 +0200

    add form data

--- index.js
+++ index.js
@@ -6,17 +6,54 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.use(express.json({ limit: '10mb' })); //?
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/generate-pdf', async (req, res) => {
-    const { content } = req.body;
+    console.log("🚀 ~ app.post ~ req.body:", req.body);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const filePath = `file:${path.join(__dirname, 'public', 'index.html')}`;
     await page.goto(filePath);
+
+    await page.evaluate((body) => {
+        document.getElementById('nazwa_jednostki').value = body.nazwa_jednostki;
+        document.getElementById('nazwa_zamowienia_text').textContent = body.nazwa_zamowienia;
+        document.getElementById(body.rodzaj_zamowienia).checked = true;
+        rodzajeHandler();
+
+        document.getElementById('glowny_cpv').value = body.glowny_cpv;
+        document.getElementById('dodatkowe_cpv_text').textContent = body.dodatkowe_cpv;        
+        document.getElementById('kategoria_uslug').value = body.kategoria_uslug;
+
+        document.getElementById(body.plan_zamowien).checked = true;
+        planZamowienHandler();
+
+        document.getElementById('plan_zamowien_rok').value = body.plan_zamowien_rok;
+        document.getElementById('plan_zamowien_oznaczenia_text').textContent = body.plan_zamowien_oznaczenia;
+        document.getElementById('plan_zamowien_wartosci_text').textContent = body.plan_zamowien_wartosci;
+
+        document.getElementById('wartosc_zamowienia').value = body.wartosc_zamowienia;
+        document.getElementById('wartosc_zamowienia_euro').value = body.wartosc_zamowienia_euro;
+
+        document.getElementById(body.zamowienie_pzp).checked = true;
+        zamowieniePzpHandler();
+
+        document.getElementById('zamowienie_pzp_kwota_text').textContent = body.zamowienie_pzp_kwota;
+
+        document.getElementById('dzien_zamowienia').value = body.dzien_zamowienia;
+        document.getElementById('podstawa_ust_wartosci_text').textContent = body.podstawa_ust_wartosci;
+        document.getElementById('wartosc_brutto').value = body.wartosc_brutto;
+        document.getElementById('kwota_przeznaczona_calosc').value = body.kwota_przeznaczona_calosc;
+        document.getElementById('kwota_przeznaczona_zrodlo_text').textContent = body.kwota_przeznaczona_zrodlo;
+        document.getElementById('kwota_przeznaczona').value = body.kwota_przeznaczona;
+        document.getElementById('termin_wykonania_text').textContent = body.termin_wykonania;
+        document.getElementById('informacje_dodatkowe_text').textContent = body.informacje_dodatkowe;
+        document.getElementById('zalaczniki_text').textContent = body.zalaczniki;
+
+    }, req.body);
 
     // const htmlContent = `
     //     <!DOCTYPE html>
@@ -61,7 +98,7 @@ app.post('/generate-pdf', async (req, res) => {
 
     res.set({
         'Content-Type': 'application/pdf',
-        'Content-Disposition': `attachment; filename=ZP-${Math.random().toString(36).substring(7)}.pdf`,
+        'Content-Disposition': 'attachment; filename=ZP.pdf',
         'Content-Length': pdfBuffer.length,
     });
 
--- public/script.js
+++ public/script.js
@@ -32,27 +32,15 @@ planZamowienHandler();
 const admin = document.getElementById("admin");
 const zamowieniePzpTak = document.getElementById("zamowienie_pzp_tak");
 const zamowieniePzpNie = document.getElementById("zamowienie_pzp_nie");
-const zamowieniePzpKwota = document.getElementById("zamowienie_pzp_kwota");
+const zamowieniePzpKwota = document.getElementById("zamowienie_pzp_kwota_text");
 function zamowieniePzpHandler() {
-    if (!admin.checked) {
-        return;
-    }
-    
     zamowieniePzpKwota.classList.toggle("input", zamowieniePzpTak.checked);
     zamowieniePzpKwota.contentEditable = zamowieniePzpTak.checked;
 }
 
-
-document.getElementById("container").addEventListener("submit", (e) => {
-    e.preventDefault();
-    document.getElementById("nazwa_zamowienia").value = document.getElementById("nazwa_zamowienia_text").innerText;
-    console.log(document.getElementById("nazwa_zamowienia").value);
-});
-
-
 document.querySelectorAll('#dodatkowe_cpv_text, ' 
                         + '#termin_wykonania_text, '
-                        + '#kwota_przeznaczona, #kwota_przeznaczona_1, #kwota_przeznaczona_2, '
+                        + '#kwota_przeznaczona_1, #kwota_przeznaczona_2, '
                         + '#wartosc_brutto_1, #wartosc_brutto_2, #wartosc_zamowienia_1, '
                         + '#wartosc_zamowienia_2, #wartosc_zamowienia_euro_1,  #wartosc_zamowienia_euro_2'
                     ).forEach((e) => {
@@ -64,7 +52,7 @@ document.querySelectorAll('#dodatkowe_cpv_text, '
     }
     e.addEventListener('keydown', dontAllowLinebreaks);
 });
-document.querySelectorAll('#nazwa_zamowienia_text, #podstawa_ust_wartosci_text, #zalaczniki_text, '
+document.querySelectorAll('#nazwa_zamowienia_text, #podstawa_ust_wartosci_text, #zalaczniki_text, #kwota_przeznaczona_zrodlo_text,'
                         + '#informacje_dodatkowe_text, '
                         + '#kwota_przeznaczona_nazwa_1, #kwota_przeznaczona_zrodlo_1, '
                         + '#kwota_przeznaczona_nazwa_2, #kwota_przeznaczona_zrodlo_2, '
@@ -90,3 +78,42 @@ function adminHandler() {
     zamowieniePzpNie.disabled = !admin.checked;
     zamowieniePzpKwota.disabled = !admin.checked;
 }
+
+
+document.getElementById('download').addEventListener('click', async () => {
+    document.getElementById('nazwa_zamowienia').value = document.getElementById('nazwa_zamowienia_text').innerText;
+    document.getElementById('dodatkowe_cpv').value = document.getElementById('dodatkowe_cpv_text').innerText;
+    document.getElementById('plan_zamowien_oznaczenia').value = document.getElementById('plan_zamowien_oznaczenia_text').innerText;
+    document.getElementById('plan_zamowien_wartosci').value = document.getElementById('plan_zamowien_wartosci_text').innerText;
+    document.getElementById('zamowienie_pzp_kwota').value = document.getElementById('zamowienie_pzp_kwota_text').innerText;
+    document.getElementById('podstawa_ust_wartosci').value = document.getElementById('podstawa_ust_wartosci_text').innerText;
+    document.getElementById('kwota_przeznaczona_zrodlo').value = document.getElementById('kwota_przeznaczona_zrodlo_text').innerText;
+    document.getElementById('termin_wykonania').value = document.getElementById('termin_wykonania_text').innerText;
+    document.getElementById('informacje_dodatkowe').value = document.getElementById('informacje_dodatkowe_text').innerText;
+    document.getElementById('zalaczniki').value = document.getElementById('zalaczniki_text').innerText;
+
+    const form = document.getElementById('container');
+
+    const formData = new FormData(form);
+
+    const disabledInputs = document.querySelectorAll('.input-box:disabled, .checkbox-input:disabled:checked');
+    disabledInputs.forEach((e) => {
+        formData.append(e.name, e.value);
+    });
+
+    const formBody = new URLSearchParams(formData).toString();
+
+    const response = await fetch('/generate-pdf', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formBody
+    });
+    
+    const blob = await response.blob();
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'wniosek.pdf';
+    link.click();
+});
