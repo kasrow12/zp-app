92d0b9cfb6ab878275fbc96414b50487afca2730
Maks Kowalski <71663613+kasrow12@users.noreply.github.com>
2025-06-11 11:09:40 +0200

    Update 16, add footer

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/generate-pdf", async (req, res) => {
 
             document.getElementById("checkbox_16_1").checked = body.checkbox_16_1 === "on";
             document.getElementById("uzasadnienie_prawne_1_text").textContent = body.uzasadnienie_prawne_1.trim();
-            document.getElementById("uzasadnienie_faktyczne_1_span").textContent = body.uzasadnienie_faktyczne_1.trim();
+            document.getElementById("uzasadnienie_faktyczne_1_text").textContent = body.uzasadnienie_faktyczne_1.trim();
 
             if (body.wylaczenie_stosowanie !== "0") {
                 document.getElementById("16_1_wylaczenie").classList.toggle("strike");
@@ -69,9 +69,9 @@ app.post("/generate-pdf", async (req, res) => {
             }
 
             document.getElementById("checkbox_16_2").checked = body.checkbox_16_2 === "on";
-            document.getElementById("tryb_udzielenia_span").textContent = body.tryb_udzielenia.trim();
-            document.getElementById("uzasadnienie_prawne_2_span").textContent = body.uzasadnienie_prawne_2.trim();
-            document.getElementById("uzasadnienie_faktyczne_2_span").textContent = body.uzasadnienie_faktyczne_2.trim();
+            document.getElementById("tryb_udzielenia_text").textContent = body.tryb_udzielenia.trim();
+            document.getElementById("uzasadnienie_prawne_2_text").textContent = body.uzasadnienie_prawne_2.trim();
+            document.getElementById("uzasadnienie_faktyczne_2_text").textContent = body.uzasadnienie_faktyczne_2.trim();
 
             document.getElementById("osoba_wnioskujaca_text").textContent = body.osoba_wnioskujaca.trim();
         }, req.body);
